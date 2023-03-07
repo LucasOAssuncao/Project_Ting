ddf321cc507c79aeea5e9e4d8718a7717097a269
Lucas Otoni <lotoni.vivo@gmail.com>
2023-03-07 16:07:53 -0300

    feature: word_search implementada

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,25 @@
+import os
+import io
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    occurrences = []
+    for i in range(len(instance)):
+        item = instance.search(i)
+        lines_found = []
+
+        for j, line in enumerate(item["linhas_do_arquivo"], start=1):
+            if word.lower() in line.lower():
+                lines_found.append({"linha": j})
+
+        if lines_found:
+            occurrences.append({
+                "palavra": word,
+                "arquivo": item["nome_do_arquivo"],
+                "ocorrencias": lines_found
+            })
+
+    return occurrences
 
 
 def search_by_word(word, instance):
