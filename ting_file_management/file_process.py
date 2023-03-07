5223ba7688a2ebaba2d4e1cf8b32d8e97dfa2182
Lucas Otoni <lotoni.vivo@gmail.com>
2023-03-07 14:48:33 -0300

    feat: file_process implementada

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,22 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for file_dict in instance.queue:
+        if file_dict['nome_do_arquivo'] == path_file:
+            return
+    
+    archive_lines = txt_importer(path_file)
+    num_lines = len(archive_lines)
+    
+    file_dict = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": num_lines,
+        "linhas_do_arquivo": archive_lines
+    }
+    
+    instance.enqueue(file_dict)
+    print(file_dict)
 
 
 def remove(instance):
