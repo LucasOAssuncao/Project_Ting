4f19964bd54d775c2cbda0f5da210fe55df1b6fa
Lucas Otoni <lotoni.vivo@gmail.com>
2023-03-07 14:03:00 -0300

    feat: queue implementado

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,19 +1,23 @@
 from ting_file_management.abstract_queue import AbstractQueue
-# ..
+from queue import deque
 
 
 class Queue(AbstractQueue):
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.queue = deque()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.queue)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self.queue.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if len(self.queue) == 0:
+            raise IndexError("Queue is empty")
+        return self.queue.popleft()
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if index < 0 or index >= len(self.queue):
+            raise IndexError("Invalid index")
+        return self.queue[index]
